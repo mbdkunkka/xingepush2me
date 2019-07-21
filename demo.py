41894ff910d8a9a3589527435f329766de4d97e7
migrantcoder <migrantcoder@live.com>
2019-07-21 19:31:29 +0800

    改成python3 信鸽push ，运行demo.py即可

---
+++ demo.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from  xinge_push import PushAccountAndroid
+ret = PushAccountAndroid(2100134390, "8c62a882a265161cd62fba02db7c0c1b", "标题", "大家好!", "15012899840")
+print(ret)
